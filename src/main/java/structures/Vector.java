57ddeca5271e147490af8202398ccc74d5f0e796
LuisD <luis.domiciano@hotmail.com>
2024-02-24 16:03:46 -0300

    feat: should return a '[]' if students vector is empty

--- src/main/java/structures/Vector.java
+++ src/main/java/structures/Vector.java
@@ -1,22 +1,45 @@
 package structures;
 
 import models.Student;
-
 public class Vector {
 
-    private Student[] students = new Student[100];
-    private int studentsTotal = 0;
-    public void add(Student student) {
-        for (int i = 0; i < this.students.length; i++) {
-            if (this.students[i] == null) {
-                this.students[i] = student;
-                studentsTotal++;
-                break;
-            }
-        }
-    }
+  private Student[] students = new Student[100];
+  private int studentsTotal = 0;
 
-    public int size() {
-        return this.studentsTotal;
+  public void add(Student student) {
+    for (int i = 0; i < this.students.length; i++) {
+      if (this.students[i] == null) {
+          this.students[i] = student;
+          studentsTotal++;
+          break;
+      }
     }
+  }
+
+  public int size() {
+    return this.studentsTotal;
+  }
+
+  public String toString() {
+    if (!isEmpty()) {
+        this.builderStringRepresentation();
+    }
+    return "[]";
+  }
+
+  private boolean isEmpty() {
+      return this.studentsTotal == 0;
+  }
+
+  private String builderStringRepresentation() {
+    var builder = new StringBuilder();
+    builder.append("[");
+    for (int index = 0; index < this.studentsTotal - 1; index++) {
+        builder.append(this.students[index]);
+        builder.append(", ");
+    }
+    builder.append(this.students[studentsTotal-1]);
+    builder.append("]");
+    return builder.toString();
+  }
 }
