011a2ecb0c02cf329e7f76a541dab4bb30934eb5
LuisD <luis.domiciano@hotmail.com>
2024-02-24 16:01:27 -0300

    fix: code indentation for improved readability and consistency

--- src/main/java/models/Student.java
+++ src/main/java/models/Student.java
@@ -1,21 +1,24 @@
 package models;
-
 public class Student {
-    String name;
-    public Student() {}
-    public String getName() {
-        return this.name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+  
+  String name;
 
-    public String toString() {
-        return this.name;
-    }
+  public Student() {}
 
-    public boolean equals(Object object) {
-        Student student = (Student) object;
-        return this.name.equals(student.name);
-    }
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String toString() {
+    return this.name;
+  }
+
+  public boolean equals(Object object) {
+    Student student = (Student) object;
+    return this.name.equals(student.name);
+  }
 }
