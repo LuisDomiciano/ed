7aef83b5f3e599e87e7631c3195205150028eefc
LuisD <luis.domiciano@hotmail.com>
2024-02-24 14:31:07 -0300

    feat: model student

---
+++ src/main/java/models/Student.java
@@ -0,0 +1,21 @@
+package models;
+
+public class Student {
+    String name;
+    public Student() {}
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+
+    public boolean equals(Object object) {
+        Student student = (Student) object;
+        return this.name.equals(student.name);
+    }
+}
